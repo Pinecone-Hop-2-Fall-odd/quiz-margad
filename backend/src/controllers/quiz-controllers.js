cb0a3d6a8eb9e48eaea575d55e0df12c22d8287b
Маргад Отгон-Өлзий <HP230499@G303-CC0137.local>
2024-01-19 19:52:59 +0800

    commit

--- backend/src/controllers/quiz-controllers.js
+++ backend/src/controllers/quiz-controllers.js
@@ -2,54 +2,75 @@ import { quizModel } from "../models/quiz-model.js";
 import { categoriesModel } from "../models/categories-model.js";
 // export const quiz = [];
 
-export const getQuizzes = async (req, res) => { 
-    try {
-          const allQuiz = await quizModel.find();
-    res.status(200).json({ allQuiz })
-    } catch (error) {
-        res.status(405).json({ message: "error"})
-    }
-}
+export const getQuizzes = async (req, res) => {
+  try {
+    const allQuiz = await quizModel.find();
+    res.status(200).json({ allQuiz });
+  } catch (error) {
+    res.status(400).json({ message: "error" });
+  }
+};
 
 export const createQuiz = async (req, res) => {
-    try {
-         const body = req.body;
+  try {
+    const body = req.body;
 
- const quiz = await quizModel.create({
-   question: body.question,
-   answers: body.answers,
-   category: body.category
-  })
-   res.status(200).json({ quiz })
-    } catch (error) {
-        res.status(405).json({ message: "error"})
-    }
-}
+    const quiz = await quizModel.create({
+      question: body.question,
+      answers: body.answers,
+      category: body.category,
+    });
+    res.status(200).json({ quiz });
+  } catch (error) {
+    res.status(400).json({ message: "error" });
+  }
+};
 
 export const getCategories = async (req, res) => {
-    try { 
-        const categories = await categoriesModel.find();
-        res.status(200).json({categories})
-    } catch (error) {
-        res.status(403).json({error: "error"})
-    }
-}
+  try {
+    const categories = await categoriesModel.find();
+    res.status(200).json({ categories });
+  } catch (error) {
+    res.status(400).json({ error: "error" });
+  }
+};
 
 export const getOneQuiz = async (req, res) => {
-    try {
-        const params  = req.params;
-        const oneQuiz = await quizModel.find({ category: params.category });
-        res.status(200).json({ oneQuiz })
-    } catch (error) {
-        res.status(403).json({ message: "error"})
+  try {
+    const params = req.params;
+    const oneQuiz = await quizModel.find({ category: params.category });
+    res.status(200).json({ oneQuiz });
+  } catch (error) {
+    res.status(400).json({ message: "error" });
+  }
+};
+export const checkQuiz = async (req, res) => {
+  try {
+    const { answers } = req.body;
+    const params = req.params;
+
+    let isPassed = true;
+
+    const quiz = await quizModel.find({ category: params.category });
+
+    Object.keys(answers).map((question) => {
+      const answersQuestion = quiz.filter((q) => q.question === question)[0];
+      const answer = answersQuestion.answers.find(
+        ({ answer }) => answer === answers[question]
+      );
+
+      if (!answer.isCorrect) {
+        isPassed = false;
+      }
+    });
+
+    if (isPassed) {
+      res.status(200).json({ message: "success" });
+    } else {
+      res.status(400).json({ message: "failed" });
     }
-}
-// export const deleteQuiz = async (req, res) => {
-//     try {
-//         const { id } = req.params;
-//         const deleteQuiz = await quizModel.findByIdAndDelete(id);
-//         res.status(200).json({ deleteQuiz })
-//     } catch (error) {
-//         res.status(403).json({ message: "error"})
-//     }
-// }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "err" });
+  }
+};
--- backend/src/routes/quiz-routes.js
+++ backend/src/routes/quiz-routes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { createQuiz, getQuizzes, getCategories, getOneQuiz} from "../controllers/quiz-controllers.js";
+import {
+  createQuiz,
+  getQuizzes,
+  getCategories,
+  getOneQuiz,
+  checkQuiz,
+} from "../controllers/quiz-controllers.js";
 import { verifyToken } from "../middleware/auth.js";
 
 export const quizRouter = express.Router();
 
-quizRouter.post("/quiz",verifyToken, createQuiz);
-quizRouter.get("/quiz",verifyToken, getQuizzes);
+quizRouter.post("/quiz", verifyToken, createQuiz);
+quizRouter.get("/quiz", verifyToken, getQuizzes);
 quizRouter.get("/categories", getCategories);
-quizRouter.get("/quiz/:category",verifyToken, getOneQuiz);
-// quizRouter.delete("/quiz/:id", deleteQuiz);
+quizRouter.get("/quiz/:category", verifyToken, getOneQuiz);
+quizRouter.post("/quizPage/:category", verifyToken, checkQuiz);
